ea4ad2394cdc98fc9eef0b67609a7f4c8d211564
devaine <devaine1203@gmail.com>
2024-02-16 15:49:44 -0600

    Spent too much time trying to figure out how to debug a C++ program only to not use it lol

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -11,9 +11,16 @@ int main() {
      std = Standard (Pronounced "see-out")
      cout = Character Output Stream
      So std::cout = "Standard Character Output"
+     
      << = Output
+
+    std:endl = Standard End Line, so basically it moves the cursor to the next line
+    since we indicated that the line ended.
      */
-    std::cout << "Hello World!";
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "What is up?" << '\n'; // Escape Characters also work
+    std::cout << "I'm good, how about you?";
+
     
     return 0; // Indicates no issues in the program
 }
